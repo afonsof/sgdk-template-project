8d6f1e47be4093b2db13f6a225292b45ce5526b1
afonsof <afonso.franca@gmail.com>
2023-08-05 02:58:26 -0300

    indent

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <genesis.h>
 
 #ifdef DEBUG
+
 #include "tests/test.h"
+
 #endif
 
 int main(bool resetType) {
@@ -9,7 +11,7 @@ int main(bool resetType) {
   START_TESTS();
   END_TESTS();
 #endif
-    VDP_drawText("Hello World!", 0, 0);
-    JOY_init();
-    SPR_init();
+  VDP_drawText("Hello World!", 0, 0);
+  JOY_init();
+  SPR_init();
 }
